9daff7fcc76d3de7889b6f0c57962369ab6c90fe
timofeymukha <timofey.mukha@it.uu.se>
2017-09-20 15:51:26 +0200

    Added LESModel NoModel, which has nut = 0 in the internalField.

---
+++ sgsModels/NoModel.C
@@ -0,0 +1,160 @@
+/*---------------------------------------------------------------------------* \
+License
+    This file is part of libWallModelledLES.
+
+    libWallModelledLES is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libWallModelledLES is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with libWallModelledLES. 
+    If not, see <http://www.gnu.org/licenses/>.
+ \*---------------------------------------------------------------------------*/
+ 
+#include "NoModel.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace LESModels
+{
+
+// * * * * * * * * * * * * Protected Member Functions  * * * * * * * * * * * //
+
+
+template<class BasicTurbulenceModel>
+tmp<volScalarField> NoModel<BasicTurbulenceModel>::k
+(
+    const volTensorField& gradU
+) const
+{
+    return tmp<volScalarField>
+    (
+        new volScalarField
+        (
+            IOobject
+            (
+                IOobject::groupName("k", this->U_.group()),
+                this->runTime_.timeName(),
+                this->mesh_
+            ),
+            this->mesh_,
+            dimensionedScalar("k", dimensionSet(0, 2, -2, 0, 0, 0 ,0), 0.0),
+            this->nut_.boundaryField().types()
+        )
+    );
+}
+
+
+template<class BasicTurbulenceModel>
+void NoModel<BasicTurbulenceModel>::correctNut()
+{
+    this->nut_.correctBoundaryConditions();
+
+    BasicTurbulenceModel::correctNut();
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+template<class BasicTurbulenceModel>
+NoModel<BasicTurbulenceModel>::NoModel
+(
+    const alphaField& alpha,
+    const rhoField& rho,
+    const volVectorField& U,
+    const surfaceScalarField& alphaRhoPhi,
+    const surfaceScalarField& phi,
+    const transportModel& transport,
+    const word& propertiesName,
+    const word& type
+)
+:
+    LESeddyViscosity<BasicTurbulenceModel>
+    (
+        type,
+        alpha,
+        rho,
+        U,
+        alphaRhoPhi,
+        phi,
+        transport,
+        propertiesName
+    )
+{
+    if (type == typeName)
+    {
+        this->nut_ = 
+            dimensionedScalar("nut", dimensionSet(0, 2, -1, 0, 0, 0 ,0), 0.0);
+        correctNut();
+        this->printCoeffs(type);
+    }
+}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+template<class BasicTurbulenceModel>
+bool NoModel<BasicTurbulenceModel>::read()
+{
+    if (LESeddyViscosity<BasicTurbulenceModel>::read())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+template<class BasicTurbulenceModel>
+tmp<volScalarField> NoModel<BasicTurbulenceModel>::epsilon() const
+{
+    volScalarField k(this->k(fvc::grad(this->U_)));
+    return tmp<volScalarField>
+    (
+        new volScalarField
+        (
+            IOobject
+            (
+                IOobject::groupName("epsilon", this->U_.group()),
+                this->runTime_.timeName(),
+                this->mesh_,
+                IOobject::NO_READ,
+                IOobject::NO_WRITE
+            ),
+            this->mesh_,
+            dimensionedScalar
+            (
+                "epsilon",
+                dimensionSet(0, 2, -3, 0, 0, 0 ,0),
+                0.0
+            ),
+            k.boundaryField().types()
+        )
+    );
+}
+
+
+template<class BasicTurbulenceModel>
+void NoModel<BasicTurbulenceModel>::correct()
+{
+    LESeddyViscosity<BasicTurbulenceModel>::correct();
+    correctNut();
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace LESModels
+} // End namespace Foam
+
+// ************************************************************************* //
