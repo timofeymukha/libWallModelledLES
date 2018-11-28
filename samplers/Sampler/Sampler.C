a77ea7d047d6f88fe0778fd2994dd0dd23cf2024
Timofey Mukha <timofey@chalmers.se>
2018-11-28 13:44:04 +0100

    Fix type #if to #ifdef in Sampler.c

--- samplers/Sampler/Sampler.C
+++ samplers/Sampler/Sampler.C
@@ -107,7 +107,7 @@ void Foam::Sampler::createIndexList()
 
     labelHashSet patchIDs{patch().index()};
 
-#if FOAM_WALLDIST_CONSTRUCTOR_ACCEPTS_METHOD
+#ifdef FOAM_WALLDIST_CONSTRUCTOR_ACCEPTS_METHOD
     wallDist distance(mesh_, "meshWave", patchIDs, "wall");
 #else
     wallDist distance(mesh_, patchIDs, "wall");
