5e6ff3012ee4e9ffddb1deee8dee93fdf4d825c3
Timofey Mukha <timofey@chalmers.se>
2019-05-06 17:49:16 +0200

    Some work on SingleCellSampler testing

--- tests/samplers/SingleCellSampler/testSingleCellSampler.C
+++ tests/samplers/SingleCellSampler/testSingleCellSampler.C
@@ -5,30 +5,37 @@
 #undef Log
 #include "gmock/gmock.h"
 
+
 class SingleCellSamplerTest : public ::testing::Test
 {
     public:
-        Foam::autoPtr<Foam::fvMesh> mesh_;
+
+        //argList * argsPtr;
+        //autoPtr<Time> runTimePtr;
+        //autoPtr<fvMesh> meshPtr;
 
         SingleCellSamplerTest()
+        //:
+            //argsPtr(nullptr),
+            //runTimePtr(nullptr),
+            //meshPtr(nullptr)
         {
             //extern int mainArgc;
             //extern char** mainArgv;
-            //int argc = mainArgc;
-            //char** argv = mainArgv;
 
-            //system("cp -r ../../testCases/channel_flow/system .");
-            //system("cp -r ../../testCases/channel_flow/constant .");
-            //system("cp -r ../../testCases/channel_flow/0 .");
+            system("cp -r ../../testCases/channel_flow/system .");
+            system("cp -r ../../testCases/channel_flow/constant .");
+            system("cp -r ../../testCases/channel_flow/0 .");
 
-            Foam::argList::noBanner();
-            //Foam::argList args(argc, argv);
-            //Foam::Time runTime(Foam::Time::controlDictName, args);
-            //mesh_.reset
+            //argsPtr = new argList(mainArgc, mainArgv);
+            //runTimePtr.reset(new Time(Foam::Time::controlDictName, *argsPtr));
+            //Time & runTime = runTimePtr();
+
+            //meshPtr.reset
             //(
-                //new Foam::fvMesh
+                //new fvMesh
                 //(
-                    //Foam::IOobject
+                    //IOobject
                     //(
                         //Foam::fvMesh::defaultRegion,
                         //runTime.timeName(),
@@ -36,36 +43,56 @@ class SingleCellSamplerTest : public ::testing::Test
                         //Foam::IOobject::MUST_READ
                     //)
                 //)
-           //);
+            //);
+
+
         }
 
         ~SingleCellSamplerTest()
         {
-            Info << "helj" <<nl;
-        }
-
-        void TearDown() override
-        {
-            Info << "tear" << nl;
+            system("rm -r system");
+            system("rm -r constant");
+            system("rm -r 0");
         }
 };
 
 TEST_F(SingleCellSamplerTest, FullConstructor)
 {
-            extern int mainArgc;
-            extern char** mainArgv;
-            int argc = mainArgc;
-            char** argv = mainArgv;
-#include "setRootCase.H"
-            Info << "hi" << nl;
-    //const fvPatch & patch = mesh_->boundary()["bottomWall"];
-    //SingleCellSampler sampler("SingleCellSampler", patch, 3.0);
+    extern int mainArgc;
+    extern char** mainArgv;
 
-    //ASSERT_EQ(sampler.averagingTime(), 3.0);
-    //ASSERT_EQ(&sampler.patch(), &patch);
-    //ASSERT_EQ(sampler.indexList().size(), patch.size());
-    //ASSERT_EQ(sampler.lengthList().size(), patch.size());
-    //ASSERT_EQ(sampler.h().size(), patch.size());
+    argList * argsPtr = new argList(mainArgc, mainArgv);
+    argList & args = *argsPtr;
+    Time runTime(Foam::Time::controlDictName, args);
+#include "createMesh.H"
+
+    mesh.time().store
+    (
+        new volScalarField
+        (
+            IOobject
+            (
+                "h",
+                mesh.time().timeName(),
+                mesh,
+                IOobject::MUST_READ,
+                IOobject::AUTO_WRITE
+            ),
+            mesh
+        )
+    );
+
+    const fvPatch & patch = mesh.boundary()["bottomWall"];
+    SingleCellSampler sampler("SingleCellSampler", patch, 3.0);
+
+    ASSERT_EQ(&sampler.patch(), &patch);
+    ASSERT_EQ(sampler.averagingTime(), 3.0);
+    ASSERT_EQ(&sampler.mesh(), &mesh);
+    ASSERT_EQ(sampler.indexList().size(), patch.size());
+    ASSERT_EQ(sampler.lengthList().size(), patch.size());
+    ASSERT_EQ(sampler.h().size(), patch.size());
+
+    EXPECT_EXIT(delete argsPtr, ::testing::ExitedWithCode(0), "");
 }
 
 int mainArgc;
@@ -73,6 +100,8 @@ char** mainArgv;
 
 int main(int argc, char **argv)
 {
+    Foam::argList::noBanner();
+
     ::testing::InitGoogleMock(&argc, argv);
     ::testing::InitGoogleTest(&argc, argv);
 
