0fdf27bed8b702baa5203377735443178e22fc1f
Timofey Mukha <timofey@chalmers.se>
2019-05-09 17:56:36 +0200

    Add tets for SampledVelocityField

--- tests/samplers/SampledField/testSampledVelocityField.C
+++ tests/samplers/SampledField/testSampledVelocityField.C
@@ -1,5 +1,6 @@
 #include "codeRules.H"
 #include "fvCFD.H"
+#include "scalarListIOList.H"
 #include "SampledVelocityField.H"
 #undef Log
 #include "gtest.h"
@@ -20,4 +21,129 @@ TEST_F(SampledVelocityTest, Constructor)
 
     const fvPatch & patch = mesh.boundary()["bottomWall"];
     SampledVelocityField sampledField(patch);
+}
+
+
+TEST_F(SampledVelocityTest, Clone)
+{
+    extern argList * mainArgs;
+    const argList & args = *mainArgs;
+    Time runTime(Foam::Time::controlDictName, args);
+
+    autoPtr<fvMesh> meshPtr = createMesh(runTime);
+    const fvMesh & mesh = meshPtr();
+
+    const fvPatch & patch = mesh.boundary()["bottomWall"];
+    SampledVelocityField sampledField(patch);
+
+    autoPtr<SampledVelocityField> clone(sampledField.clone());
+}
+
+
+TEST_F(SampledVelocityTest, NDims)
+{
+    extern argList * mainArgs;
+    const argList & args = *mainArgs;
+    Time runTime(Foam::Time::controlDictName, args);
+    autoPtr<fvMesh> meshPtr = createMesh(runTime);
+    const fvMesh & mesh = meshPtr();
+
+    const fvPatch & patch = mesh.boundary()["bottomWall"];
+    SampledVelocityField sampledField(patch);
+
+    ASSERT_EQ(sampledField.nDims(), 3);
+}
+
+
+TEST_F(SampledVelocityTest, Name)
+{
+    extern argList * mainArgs;
+    const argList & args = *mainArgs;
+    Time runTime(Foam::Time::controlDictName, args);
+
+    autoPtr<fvMesh> meshPtr = createMesh(runTime);
+    const fvMesh & mesh = meshPtr();
+
+    const fvPatch & patch = mesh.boundary()["bottomWall"];
+    SampledVelocityField sampledField(patch);
+
+    ASSERT_EQ(sampledField.name(), "U");
+}
+
+
+TEST_F(SampledVelocityTest, RegisterFieldsZero)
+{
+    extern argList * mainArgs;
+    const argList & args = *mainArgs;
+    Time runTime(Foam::Time::controlDictName, args);
+
+    autoPtr<fvMesh> meshPtr = createMesh(runTime);
+    const fvMesh & mesh = meshPtr();
+
+    const fvPatch & patch = mesh.boundary()["bottomWall"];
+    createWallModelSubregistry(mesh, patch);
+
+    SampledVelocityField sampledField(patch);
+
+    sampledField.registerFields(patch.faceCells());
+
+    // Assert we registred the field in the registry
+    ASSERT_TRUE(sampledField.db().foundObject<scalarListIOList>("U"));
+
+    const scalarListIOList & sampledFieldIOobject = sampledField.db().lookupObject<scalarListIOList>("U");
+
+    forAll(sampledFieldIOobject, i)
+    {
+        forAll(sampledFieldIOobject[i], j)
+        {
+            ASSERT_EQ(sampledFieldIOobject[i][j], 0);
+        }
+    }
+}
+
+
+TEST_F(SampledVelocityTest, RegisterFieldsInitialize)
+{
+    extern argList * mainArgs;
+    const argList & args = *mainArgs;
+    Time runTime(Foam::Time::controlDictName, args);
+
+    autoPtr<fvMesh> meshPtr = createMesh(runTime);
+    const fvMesh & mesh = meshPtr();
+
+    createVelocityField(mesh);
+    volVectorField & U = mesh.lookupObjectRef<volVectorField>("U");
+
+    // Init U to something varying
+    U.primitiveFieldRef() = mesh.C();
+
+    const fvPatch & patch = mesh.boundary()["bottomWall"];
+    createWallModelSubregistry(mesh, patch);
+
+    SampledVelocityField sampledField(patch);
+
+    sampledField.registerFields(patch.faceCells());
+
+    // Assert we registred the field in the registry
+    ASSERT_TRUE(sampledField.db().foundObject<scalarListIOList>("U"));
+
+    const scalarListIOList & sampledFieldIOobject = sampledField.db().lookupObject<scalarListIOList>("U");
+
+    forAll(sampledFieldIOobject, i)
+    {
+        forAll(sampledFieldIOobject[i], j)
+        {
+            if (j == 1)
+            {
+                ASSERT_EQ(sampledFieldIOobject[i][j], 0);
+            }
+            else
+            {
+                ASSERT_EQ(sampledFieldIOobject[i][j], U[patch.faceCells()[i]][j]);
+            }
+        }
+    }
+
+    //sampledFieldIOobject = sampledFied.db().
+
 }
