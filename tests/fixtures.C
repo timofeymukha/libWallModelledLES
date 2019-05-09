46aff5105f34e758024c819fbce138e68737aea2
Timofey Mukha <timofey@chalmers.se>
2019-05-09 17:55:43 +0200

    Add fixtures

--- tests/fixtures.C
+++ tests/fixtures.C
@@ -19,6 +19,27 @@ void createSamplingHeightField(const Foam::fvMesh & mesh)
     );
 }
 
+
+void createVelocityField(const Foam::fvMesh & mesh)
+{
+    mesh.time().store
+    (
+        new volVectorField
+        (
+            IOobject
+            (
+                "U",
+                mesh.time().timeName(),
+                mesh,
+                IOobject::MUST_READ,
+                IOobject::AUTO_WRITE
+            ),
+            mesh
+        )
+    );
+}
+
+
 Foam::autoPtr<Foam::fvMesh> createMesh(const Foam::Time & runTime)
 {
     Foam::autoPtr<Foam::fvMesh> meshPtr(nullptr);
@@ -40,3 +61,35 @@ Foam::autoPtr<Foam::fvMesh> createMesh(const Foam::Time & runTime)
     return meshPtr;
 }
 
+
+void createWallModelSubregistry(const fvMesh & mesh, const fvPatch & patch)
+{
+    if (!mesh.foundObject<objectRegistry>("wallModelSampling"))
+    {
+        objectRegistry * subObr = new objectRegistry
+        (
+            IOobject
+            (
+                "wallModelSampling",
+                mesh.time().constant(),
+                mesh,
+                IOobject::NO_READ,
+                IOobject::NO_WRITE
+            )
+        );
+        subObr->store();
+    }
+
+    objectRegistry * subObr = new objectRegistry
+    (
+        IOobject
+        (
+            patch.name(),
+            mesh.time().constant(),
+            mesh.subRegistry("wallModelSampling"),
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        )
+    );
+    subObr->store();
+}
