e71322f52c05af8055bc45b7e5f156db97418491
Timofey Mukha <timofey@chalmers.se>
2019-08-01 14:51:10 +0200

    Update compilation script

--- tests/testCompilation.sh
+++ tests/testCompilation.sh
@@ -58,9 +58,31 @@ then
     docker start of_v1906
     docker exec -w $PWD/.. of_v1906 /bin/bash -c "source /opt/OpenFOAM/setImage_v1906.sh && ./Allwclean && ./Allwmake"
 fi
-
-#docker exec -w $PWD/.. of_v1706 /bin/bash -c "source /opt/OpenFOAM/setImage_v1706.sh && ./Allwclean && ./Allwmake"
-#docker exec -w $PWD/.. of_v1712 /bin/bash -c "source /opt/OpenFOAM/setImage_v1712.sh && ./Allwclean && ./Allwmake"
-#docker exec -w $PWD/.. of_v1806 /bin/bash -c "source /opt/OpenFOAM/setImage_v1806.sh && ./Allwclean && ./Allwmake"
-#docker exec -w $PWD/.. of_v1812 /bin/bash -c "source /opt/OpenFOAM/setImage_v1812.sh && ./Allwclean && ./Allwmake"
-
+if [ $1 == "all" ]
+then
+    mkdir logs
+    docker start of_4
+    docker exec -w $PWD/.. of_4 /bin/bash -c ". /opt/openfoam4/etc/bashrc && export FOAM_USER_LIBBIN=/home/timofey/OpenFOAM/timofey-4.1/platforms/linux64GccDPInt32Opt/lib && ./Allwclean && ./Allwmake &> tests/logs/4.log"
+    docker start of_5
+    docker exec -w $PWD/.. of_5 /bin/bash -c ". /opt/openfoam5/etc/bashrc && export FOAM_USER_LIBBIN=/home/timofey/OpenFOAM/timofey-5/platforms/linux64GccDPInt32Opt/lib && ./Allwclean && ./Allwmake &> tests/logs/5.log"
+    docker start of_6
+    docker exec -w $PWD/.. of_6 /bin/bash -c ". /opt/openfoam6/etc/bashrc && export FOAM_USER_LIBBIN=/home/timofey/OpenFOAM/timofey-6/platforms/linux64GccDPInt32Opt/lib && ./Allwclean && ./Allwmake &> tests/logs/6.log"
+    docker start of_7
+    docker exec -w $PWD/.. of_7 /bin/bash -c ". /opt/openfoam7/etc/bashrc && export FOAM_USER_LIBBIN=/home/timofey/OpenFOAM/timofey-6/platforms/linux64GccDPInt32Opt/lib && ./Allwclean && ./Allwmake &> tests/logs/7.log"
+    docker start of_v30plus
+    docker exec -w $PWD/.. of_v30plus /bin/bash -c "source /opt/OpenFOAM/OpenFOAM-v3.0+/etc/bashrc && ./Allwclean && ./Allwmake &> tests/logs/3+.log"
+    docker start of_v1606
+    docker exec -w $PWD/.. of_v1606 /bin/bash -c "source /opt/OpenFOAM/setImage_v1606+ && ./Allwclean && ./Allwmake &> tests/logs/1606.log"
+    docker start of_v1612
+    docker exec -w $PWD/.. of_v1612 /bin/bash -c "source /opt/OpenFOAM/setImage_v1612+ && ./Allwclean && ./Allwmake &> tests/logs/1612.log"
+    docker start of_v1706
+    docker exec -w $PWD/.. of_v1706 /bin/bash -c "source /opt/OpenFOAM/setImage_v1706.sh && ./Allwclean && ./Allwmake &> tests/logs/1706.log"
+    docker start of_v1712
+    docker exec -w $PWD/.. of_v1712 /bin/bash -c "source /opt/OpenFOAM/setImage_v1712.sh && ./Allwclean && ./Allwmake &> tests/logs/1712.log"
+    docker start of_v1806
+    docker exec -w $PWD/.. of_v1806 /bin/bash -c "source /opt/OpenFOAM/setImage_v1806.sh && ./Allwclean && ./Allwmake &> tests/logs/1806.log"
+    docker start of_v1812
+    docker exec -w $PWD/.. of_v1812 /bin/bash -c "source /opt/OpenFOAM/setImage_v1812.sh && ./Allwclean && ./Allwmake &> tests/logs/1812.log"
+    docker start of_v1906
+    docker exec -w $PWD/.. of_v1906 /bin/bash -c "source /opt/OpenFOAM/setImage_v1906.sh && ./Allwclean && ./Allwmake &> tetts/logs/1906.log"
+fi
